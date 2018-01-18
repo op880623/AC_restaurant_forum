50d4cdef6bc325dcd42858797afd89e8bab13aed
op880623 <op880623@gmail.com>
2018-01-18 17:32:32 +0800

    rails g controller users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  before_action :set_user
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user == current_user
+      if @user.update(user_params)
+        flash[:notice] = "User was seccessfully updated."
+        redirect_to user_path(@user)
+      else
+        flash.now[:alert] = "User was failed to update."
+        render :edit
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+  end
+
+end
