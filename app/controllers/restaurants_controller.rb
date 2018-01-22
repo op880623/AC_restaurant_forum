4f2a289ede3958ae95955717c6be8b24b6b53070
op880623 <op880623@gmail.com>
2018-01-22 15:27:33 +0800

    add actions on RestaurantController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :dashboard]
+  before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite]
 
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
@@ -17,6 +17,17 @@ class RestaurantsController < ApplicationController
   end
 
   def dashboard
+  end
+
+  def favorite
+    @restaurant.favorites.create!(user: current_user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def unfavorite
+    favorites = Favorite.where(restaurant: @restaurant, user: current_user)
+    favorites.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
   private
