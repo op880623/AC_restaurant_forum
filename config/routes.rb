03a28bd148a1e09f709ec0222aa8bc5273a099bc
op880623 <op880623@gmail.com>
2018-01-24 15:41:26 +0800

    add followship routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :categories, only: [:show]
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
       delete :unlike
     end
   end
+  resources :followships, only: [:create, :destroy]
 
   namespace :admin do
     root "restaurants#index"
